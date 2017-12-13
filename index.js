b4761864e7b90f7f2968dd4f3689a2baab80d30d
张萌 <843354450@qq.com>
2017-12-13 08:02:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = function (rows_, opts) {
-    if (!opts) opts = {};
-    var hsep = opts.hsep === undefined ? '  ' : opts.hsep;
-    var align = opts.align || [];
+module.exports = function (rows_, opts) {                  //暴露的函数作为第三方模块引用,参数为数组rows_，对象opts
+    if (!opts) opts = {};                                  //判断opts是否存在,不存在，则赋值一个空对象
+    var hsep = opts.hsep === undefined ? '  ' : opts.hsep; //列之间的分隔符，没有定义就默认为' ',
+    var align = opts.align || [];                          //列的对齐类型数组，没有定义就是空数组
     var stringLength = opts.stringLength || function (s) { return String(s).length; } ;
-    
+                                                          //字符串长度，没有定义就计算字符串长度并返回。
     var dotsizes = reduce(rows_, function (acc, row) {
         forEach(row, function (c, ix) {
             var n = dotindex(c);
@@ -31,7 +31,7 @@ module.exports = function (rows_, opts) {
         });
         return acc;
     }, []);
-    
+    //是对l/r/c格式化的处理过程，通过判断，添加每列之间的分隔符,返回一个处理完成的文本表。
     return map(rows, function (row) {
         return map(row, function (c, ix) {
             var n = (sizes[ix] - stringLength(c)) || 0;
@@ -49,32 +49,32 @@ module.exports = function (rows_, opts) {
 };
 
 function dotindex (c) {
-    var m = /\.[^.]*$/.exec(c);
-    return m ? m.index + 1 : c.length;
+    var m = /\.[^.]*$/.exec(c);            //正则表达式匹配
+    return m ? m.index + 1 : c.length;     //m存在，返回匹配项索引值的下一位，如果不存在，返回传入数组的长度。
 }
 
-function reduce (xs, f, init) {
-    if (xs.reduce) return xs.reduce(f, init);
+function reduce (xs, f, init) {            
+    if (xs.reduce) return xs.reduce(f, init);     //判断数组对象是否有 reduce 方法，有就直接调用原生的 reduce 方法，没有就执行下面
     var i = 0;
-    var acc = arguments.length >= 3 ? init : xs[i++];
+    var acc = arguments.length >= 3 ? init : xs[i++];   //如果传入实参个数大于等于3，则等于init,否则等于xs[i++]
     for (; i < xs.length; i++) {
-        f(acc, xs[i], i);
+        f(acc, xs[i], i);                           //遍历数组xs,每一项调用f函数方法
     }
     return acc;
 }
 
 function forEach (xs, f) {
-    if (xs.forEach) return xs.forEach(f);
+    if (xs.forEach) return xs.forEach(f);        //判断数组对象是否有forEach方法，有就直接调用原生的forEach方法，没有就执行下面数组xs的遍历
     for (var i = 0; i < xs.length; i++) {
-        f.call(xs, xs[i], i);
+        f.call(xs, xs[i], i);                    //数组遍历，每一项调用f函数方法
     }
 }
 
-function map (xs, f) {
-    if (xs.map) return xs.map(f);
-    var res = [];
+function map (xs, f) {        
+    if (xs.map) return xs.map(f);             //首先判断数组对象是否有map方法，有就直接调用原生的map方法，没有就执行下面
+    var res = [];                             //定义新的数组res
     for (var i = 0; i < xs.length; i++) {
-        res.push(f.call(xs, xs[i], i));
+        res.push(f.call(xs, xs[i], i));      //将数组xs边遍历边处理,每一项调用f函数方法，并将结果写入新数组res中
     }
     return res;
 }
